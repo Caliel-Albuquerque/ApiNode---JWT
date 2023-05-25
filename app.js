35123c70eebb59a6fa5819dda422582e2827db84
Caliel-Albuquerque <75955275+Caliel-Albuquerque@users.noreply.github.com>
2023-05-25 17:17:21 -0300

    ApiJWT

---
+++ app.js
@@ -0,0 +1,176 @@
+//Imports
+require("dotenv").config()
+const express = require("express")
+const mongoose = require("mongoose")
+const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken")
+
+const app = express()
+
+//Accept JSON response 
+app.use(express.json())
+
+//Models 
+const User = require("./models/User")
+
+//Public Route 
+app.get("/", (req, res) => {
+  res.status(200).json({msg: "Success"})
+})
+
+//Private Route
+app.get('/user/:id',checkToken, async (req, res) => {
+  const id = req.params.id
+
+  // check if user exists 
+  const user = await User.findById(id, '-password')
+
+  if(!user){
+    return res.status(404).json({msg:'Usurio não encontrado'})
+  }
+
+  res.status(200).json({msg: user })
+})
+
+function checkToken(req, res, next){
+  const authHeader = req.headers["authorization"]
+  const token = authHeader && authHeader.split(" ")[1]
+
+  if(!token){
+    return res.status(403).json({msg: 'Acesso negado!'})
+  }
+
+  try{
+
+    const secret = process.env.SECRET
+
+    jwt.verify(token, secret)
+
+    next()
+  } catch(err){
+    console.log(err)  
+    res.status(500).json({msg: 'Token invalido'}) 
+  }
+}
+
+//Register User 
+app.post("/auth/register", async(req, res) => {
+
+  const {name, email, password, confirmpassword, ponto: { data, entrada, saida }, 
+   ferias: { inicio, fim, status, dias } } = req.body 
+
+  //validate
+
+  if(!name){
+   return res.status(422).json({msg:"O nome deve ser informado"})
+  }
+
+  if(!email){
+    return res.status(422).json({msg:"O email deve ser informado"})
+  }
+
+  if(!password){
+    return res.status(422).json({msg:"A senha é obrigatoria"})
+  }
+
+  if(password != confirmpassword){
+    return res.status(422).json({msg:"As senhas estão diferentes"})
+  }
+
+  //check if user is already registered
+  const userExist = await User.findOne({email: email})
+
+  if(userExist){
+    return res.status(422).json({msg:"Esse email já existe"})
+  }
+
+  //create password
+  const salt = await bcrypt.genSalt(12)
+  const passwordHash = await bcrypt.hash(password, salt)
+
+
+  //create user 
+
+  const user = new User({
+    name,
+    password: passwordHash,
+    email,
+    ferias: { inicio, fim, status, dias},
+    ponto: { data, entrada, saida}
+  })
+
+  try{
+
+    await user.save()
+
+    res.status(201).json({msg: 'usuario criado com sucesso'})
+
+
+  }catch(err){
+    console.error(err)
+    res.status(500).json({msg: 'aconteceu um erro, tente novamente mais tarde'})
+  }
+
+})
+
+//Login user 
+
+app.post('/auth/user', async(req, res) => {
+  const {email, password} = req.body
+
+  //Validate
+
+  if(!email){
+    return res.status(422).json({msg: 'Email é obrigatorio'})
+  }
+
+  if(!password){
+    return res.status(422).json({msg: 'Senha é obrigatorio'})
+  }
+
+  //User exist
+
+  const user = await User.findOne({email: email})
+
+  if(!user){
+    return res.status(404).json({msg: 'O usuario não existe'})
+  }
+
+  //Compare password
+
+  const checkPassword = bcrypt.compare(password, user.password)
+
+  if(!checkPassword){
+    return res.status(422).json({msg: 'Senha invalida'})
+  }
+
+
+  try{
+
+    const secret = process.env.SECRET
+
+    const token = jwt.sign({
+      id: user.id,
+
+    }, secret)
+
+    res.status(200).json({msg: 'Autenticação com sucesso', token})
+
+  }catch(err){
+    console.error(err)
+    res.status(500).json({msg: 'Algo de errado'})
+  }
+})
+
+
+//Credentials
+const dbUser = process.env.DB_USER
+const dbPassword = process.env.DB_PASS
+
+
+mongoose.connect(`mongodb+srv://${dbUser}:${dbPassword}@cluster0.q3weozz.mongodb.net/?retryWrites=true&w=majority`).then(
+  app.listen(3000),
+  console.log("Banco Conectado!")
+).catch((err) => {console.error(err)})
+
+
---
+++ models/User.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+
+const User = mongoose.model('User', {
+  name: String,
+  email: String,
+  password: String,
+  ponto: {
+    data: String,
+    entrada: String,
+    saida: String
+  },
+  ferias:{
+    inicio: String,
+    fim: String,
+    status: Boolean,
+    dias: Number,
+  }
+
+})
+
+module.exports = User
